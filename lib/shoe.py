0810ee403a84efe875941bdbb6adc2d4bda40f65
Yu Wu <yw3748@columbia.edu>
2023-07-06 17:20:36 -0400

    lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand=None, size=None, condition=None):
+        if brand is not None:
+            self.brand = brand
+        if size is not None:
+            self.size = size
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, new_size):
+        if type(new_size) == int:
+            self._size = new_size
+        else:
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self._condition = "New"
+
+    def get_condition(self):
+        return self._condition
+
+    condition = property(get_condition, cobble)
